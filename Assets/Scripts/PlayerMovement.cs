43a5800700a07e4c169cbd52b76b56d53f798be0
DaisyFragoso <fragosodaisy05@gmail.com>
2026-03-09 16:01:52 -0700

    update npc waypoints

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Awake()
     {
-        isActive = false;
+        isActive = true;
         animator = GetComponent<Animator>();
     }
 
